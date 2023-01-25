13b95cc088147572831010e2e880ce71b3e50f27
Nicolas Beaulieu <nicolas.beaulieu2000@gmail.com>
2023-01-25 15:37:10 -0500

    Finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, numDelete) {
+    const newArray = []
+    array.forEach((item) => {
+        if(!numDelete.includes(item)){
+            newArray.push(item)
+        }
+    })
 };
+
+removeFromArray([1,2,3,4],3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
-
+const sumAll = function(arraySum) {
+    let sum = 0
+    arraySum.forEach(item => {
+        sum += item
+    })
+    return sum
 };
+
+function getRange(firstNumb, lastNumb){
+    newArray = []
+    if(firstNumb < 0 || lastNumb < 0 || typeof lastNumb === 'string' || typeof firstNumb === 'string' || typeof firstNumb === 'object' || typeof lastNumb === 'object'){
+        return 'ERROR'
+    }else if(firstNumb > lastNumb){
+        for(let i = lastNumb; i <= firstNumb; i++){
+            newArray.push(i)
+        }
+    }else{
+        for(let i = firstNumb; i <= lastNumb; i++ ){
+            newArray.push(i)
+        }
+    }
+    return newArray
+}
+
+arrayToFindRange = [10, -4]
+
+arrayToSum = getRange(arrayToFindRange[0], arrayToFindRange[1] )
+
+console.log(sumAll(arrayToSum))
 
 // Do not edit below this line
 module.exports = sumAll;
