c5487d19dc0c2c271ecc4a2aa1ca2cd360e61c41
Nicolas Beaulieu <nicolas.beaulieu2000@gmail.com>
2023-01-25 08:49:41 -0500

    Exercises rréussi

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(string, numLet){
+    arrayStringReverse = []
+    for(let i = numLet; i !== 0; i--){
+        arrayStringReverse.push(string[i-1])
+    }
+    console.log(arrayStringReverse)
+    return arrayStringReverse.join('') 
+}
 
-};
+function getNumLetterInString(word){
+    return word.length
+}
+
+
+console.log(reverseString('', getNumLetterInString('')))
+
 
 // Do not edit below this line
 module.exports = reverseString;
