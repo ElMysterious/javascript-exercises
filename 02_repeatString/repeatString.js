16a02e73d244e5c448e466f307e10602a284045c
Nicolas Beaulieu <nicolas.beaulieu2000@gmail.com>
2023-01-25 06:56:27 -0500

    Exercise fini!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let arrayString = []
+    if(num < 0) return 'ERROR'
+    for(let i = 0; i < num; i++){
+        arrayString.push(string)
+     }
+    return arrayString.join('')
 };
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * 1000)
+}
+
+randomNumber = getRandomNumber()
+
+repeatString('hey', -1)
+
 
 // Do not edit below this line
 module.exports = repeatString;
