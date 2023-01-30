ab0969a92b76f112e8632bc25631e59608fad21c
Nicolas Beaulieu <nicolas.beaulieu2000@gmail.com>
2023-01-30 12:58:29 -0500

    Exercise fini

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  convertValue = (fahrenheit - 32)* 5/9
+  return Math.round(convertValue * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    newConvertValue = celsius * 9/5 +32
+    return Math.round(newConvertValue * 10) / 10
 };
+
+convertToCelsius(-100)
+convertToFahrenheit(0)
 
 // Do not edit below this line
 module.exports = {
